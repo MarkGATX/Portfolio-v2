7534dcc8fa72c9a2799aa86ba2e3d8c5bf2fe4db
Mark Gardner <themarkgardner@gmail.com>
2022-11-22 21:57:09 -0600

    Lots of minor css tweaks. Hid contact form until handler can be figured out. Added random question about me to section. Changed size of animation. Need to clean up unused images.Leaving folder structure for now until migrate to React

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,33 @@
+const quest = document.getElementById('question');
+const ans = document.getElementById('answer');
+
+
+const aboutMe = [
+    ["Favorite food?", "Mole enchiladas or sushi"], 
+    ["Favorite movie?", "Serenity"],
+    ["Star Wars or Star Trek?", "Both, all day long"],
+    ["Hogwart's House?", "Hufflepuff, Huffle-proud"],
+    ["Favorite Artist?", "John Singer Sargent"],
+    ["Dream Job?", "Starfleet Captain"],
+    ["Best Star Trek Captain?", "Captain Pike, SNW"],
+    ["Hobby?", "Board games"],
+    ["Guilty Pleasure?", "None! You shouldn't feel guilty for doing fun things."],
+    ["Favorite Pokemon?", "Eevee"],
+    ["Favorite Graphic Novel?", "Y: The Last Man"],
+    ["Current Favorite Author?", "N.K. Jemison"],
+    ["Scariest Show Ever?", "The Haunting of Hill House (Netflix)"],
+    ["Marvel or DC?","I choose both"],
+    ["Mountains or Beach?", "Mountains all day"],
+    ["Favorite Sport?", "Volleyball"],
+    ["Second Favorite Sport?", "Soccer"],
+    ["Cake or pie?", "Quit trying to make me choose!"],
+    ["Favorite Fruit?", "Strawberries"]
+]
+
+const questNum = Math.floor(Math.random() * aboutMe.length+1);
+quest.textContent="Random question: " + aboutMe[questNum][0];
+ans.textContent=aboutMe[questNum][1];
+
 const linkTarget = function (event) {
     event.preventDefault();
     console.log(event.target)
@@ -31,6 +61,9 @@ const linkTarget = function (event) {
         case 'hoa':
             window.open("https://www.youtube.com/playlist?list=PLtk51oTcFVK9U1QfOj18dAWu80QXEN9sG");
             break;
+        case 'wg':
+            window.open("https://youtu.be/MRzQGe8m_4A");
+            break;
         default:
             break;
 
@@ -42,3 +75,4 @@ let listeners = document.getElementsByClassName('projectFeat');
 for (let i = 0; i < listeners.length; i++) {
     listeners[i].addEventListener('click', linkTarget);
 }
+
