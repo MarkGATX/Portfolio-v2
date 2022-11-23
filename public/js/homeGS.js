610cd59837390957a6a7be119a8e95a50ac51d0b
Mark Gardner <themarkgardner@gmail.com>
2022-11-22 22:23:25 -0600

    adjusted z-index of animation

--- public/js/homeGS.js
+++ public/js/homeGS.js
@@ -5,8 +5,8 @@ const tl = gsap.timeline();
 
 tl.from("#animation_container", {duration:1, x:-2000 ,y:+1});
 tl.from(".header-name", {duration:1.7, x:-1500}, '-=1.5');
-tl.from('.pretend-border', {duration:3, backgroundImage: 'radial-gradient(at 70% top, rgb(230,250,252) 1%, rgb(230,250,252) 30%)'}, "-=.1");
-tl.from('.nav-item', {duration:1, y:15, opacity:0, stagger:.3}, "-=2.6");
-tl.from('.forgeBy', { duration:2, y:-20, opacity:0}, '-=2.0');
-tl.from('.fuelBy', {duration:2, y:-20, opacity:0, }, '-=1.5');
-tl.from('main', {duration:1, opacity:0}, '-=.7')
+tl.from('.pretend-border', {duration:4, backgroundImage: 'radial-gradient(at 70% top, rgb(230,250,252) 1%, rgb(230,250,252) 30%)'}, "-=.1");
+tl.from('.nav-item', {duration:1, y:15, opacity:0, stagger:.3}, "-=3.6");
+tl.from('.forgeBy', { duration:2, y:-20, opacity:0}, '-=3.0');
+tl.from('.fuelBy', {duration:2, y:-20, opacity:0, }, '-=2.5');
+tl.from('main', {duration:1, opacity:0}, '-=1.7')
